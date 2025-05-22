e2925d6959fd6639d5879156c999167efe46cffb
Matthias Uhlig <126894819+DRXD1000@users.noreply.github.com>
2025-05-22 14:35:41 +0200

    Adding custom _aget method

--- libs/langchain-mongodb/langchain_mongodb/retrievers/hybrid_search.py
+++ libs/langchain-mongodb/langchain_mongodb/retrievers/hybrid_search.py
@@ -1,6 +1,9 @@
 from typing import Annotated, Any, Dict, List, Optional
 
-from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
+from langchain_core.callbacks.manager import (
+    AsyncCallbackManagerForRetrieverRun,
+    CallbackManagerForRetrieverRun,
+)
 from langchain_core.documents import Document
 from langchain_core.retrievers import BaseRetriever
 from pydantic import Field
@@ -135,3 +138,94 @@ class MongoDBAtlasHybridSearchRetriever(BaseRetriever):
             make_serializable(res)
             docs.append(Document(page_content=text, metadata=res))
         return docs
+
+    async def _aget_relevant_documents(
+        self,
+        query: str,
+        *,
+        run_manager: AsyncCallbackManagerForRetrieverRun,
+        **kwargs: Any,
+    ) -> List[Document]:
+        """Retrieve documents that are highest scoring / most similar  to query.
+
+        Note that the same query is used in both searches,
+        embedded for vector search, and as-is for full-text search.
+
+        Args:
+            query: String to find relevant documents for
+            run_manager: The callback handler to use
+        Returns:
+            List of relevant documents
+        """
+
+        if isinstance(self.collection, Collection):
+            return await super()._aget_relevant_documents(
+                query,
+                run_manager=run_manager,
+            )
+
+        else:
+            query_vector = self.vectorstore._embedding.embed_query(query)
+
+            scores_fields = ["vector_score", "fulltext_score"]
+            pipeline: List[Any] = []
+
+            # Get the appropriate value for k.
+            default_k = self.top_k if self.top_k is not None else self.k
+            k = kwargs.get("k", default_k)
+
+            # First we build up the aggregation pipeline,
+            # then it is passed to the server to execute
+            # Vector Search stage
+            vector_pipeline = [
+                vector_search_stage(
+                    query_vector=query_vector,
+                    search_field=self.vectorstore._embedding_key,
+                    index_name=self.vectorstore._index_name,
+                    top_k=k,
+                    filter=self.pre_filter,
+                    oversampling_factor=self.oversampling_factor,
+                )
+            ]
+            vector_pipeline += reciprocal_rank_stage(
+                "vector_score", self.vector_penalty
+            )
+
+            combine_pipelines(pipeline, vector_pipeline, self.collection.name)
+
+            # Full-Text Search stage
+            text_pipeline = text_search_stage(
+                query=query,
+                search_field=self.vectorstore._text_key,
+                index_name=self.search_index_name,
+                limit=k,
+                filter=self.pre_filter,
+            )
+
+            text_pipeline.extend(
+                reciprocal_rank_stage("fulltext_score", self.fulltext_penalty)
+            )
+
+            combine_pipelines(pipeline, text_pipeline, self.collection.name)
+
+            # Sum and sort stage
+            pipeline.extend(final_hybrid_stage(scores_fields=scores_fields, limit=k))
+
+            # Removal of embeddings unless requested.
+            if not self.show_embeddings:
+                pipeline.append({"$project": {self.vectorstore._embedding_key: 0}})
+            # Post filtering
+            if self.post_filter is not None:
+                pipeline.extend(self.post_filter)
+
+            # Execution
+            cursor = await self.collection.aggregate(pipeline)  # type: ignore[arg-type]
+
+            # Formatting
+            docs = []
+            async for res in cursor:
+                text = res.pop(self.vectorstore._text_key)
+                # score = res.pop("score")  # The score remains buried!
+                make_serializable(res)
+                docs.append(Document(page_content=text, metadata=res))
+            return docs
