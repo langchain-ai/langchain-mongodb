f45dce2aeee102978027069d9a743da62daeb179
Steven Silvester <steve.silvester@mongodb.com>
2025-07-25 12:46:45 -0500

    lint

--- libs/langgraph-checkpoint-mongodb/langgraph/checkpoint/mongodb/aio.py
+++ libs/langgraph-checkpoint-mongodb/langgraph/checkpoint/mongodb/aio.py
@@ -478,7 +478,7 @@ class AsyncMongoDBSaver(BaseCheckpointSaver):
         while True:
             try:
                 yield asyncio.run_coroutine_threadsafe(
-                    anext(aiter_),
+                    anext(aiter_),  # type:ignore[arg-type]
                     self.loop,
                 ).result()
             except StopAsyncIteration:
