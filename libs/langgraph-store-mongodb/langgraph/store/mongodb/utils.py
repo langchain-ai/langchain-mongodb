aec649a45388e42a105f6f1c18c09e3ad418440b
Jeffrey A. Clark <aclark@aclark.net>
2026-01-12 23:57:10 -0500

    Use pymongo-search-utils to append_client_metadata

--- libs/langgraph-store-mongodb/langgraph/store/mongodb/utils.py
+++ libs/langgraph-store-mongodb/langgraph/store/mongodb/utils.py
@@ -7,6 +7,7 @@ from importlib.metadata import version
 
 from pymongo import MongoClient
 from pymongo.driver_info import DriverInfo
+from pymongo_search_utils import append_client_metadata
 
 DRIVER_METADATA = DriverInfo(
     name="Langgraph", version=version("langgraph-store-mongodb")
@@ -14,6 +15,4 @@ DRIVER_METADATA = DriverInfo(
 
 
 def _append_client_metadata(client: MongoClient) -> None:
-    # append_metadata was added in PyMongo 4.14.0, but is a valid database name on earlier versions
-    if callable(client.append_metadata):
-        client.append_metadata(DRIVER_METADATA)
+    append_client_metadata(client=client, driver_info=DRIVER_METADATA)
